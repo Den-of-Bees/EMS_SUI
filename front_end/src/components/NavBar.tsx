ed6cadc8b51ec9c12e6ce1e3319898ce12cb32b0
Shaun Ellis <shaun.ellis@tamu.edu>
2025-03-26 15:59:16 -0500

    Added Tailwind and a dummy navbar. centers and fleboxed the main content

---
+++ front_end/src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+
+const NavBar = () => (
+  <nav className="bg-blue-600 p-4 text-white flex gap-4">
+    <a href="#" className="hover:underline">Home</a>
+    <a href="#" className="hover:underline">Dashboard</a>
+    <a href="#" className="hover:underline">Settings</a>
+  </nav>
+);
+
+export default NavBar
